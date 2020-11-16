55991ad90a41c0cde1895e6c575427b47d70261f
Shubham Vishwakarma <shubham.vishwakarma@justdial.com>
2020-11-16 21:59:11 +0530

    Formatted Code

--- src/config/env-loader.ts
+++ src/config/env-loader.ts
@@ -1,5 +1,5 @@
-import * as dotenv from "dotenv";
-const env = process.env.NODE_ENV || "development";
+import * as dotenv from 'dotenv';
+const env = process.env.NODE_ENV || 'development';
 const dotEnvOptions = {
   path: `.env.${env}`,
 };
