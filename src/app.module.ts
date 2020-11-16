86f2ff5cd33f46eddfda7b2f4a887e66bc7ffc41
Shubham Vishwakarma <shubham.vishwakarma@justdial.com>
2020-11-16 23:24:41 +0530

    Typeorm Init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import dbConfig from './config/db-config';
 import { PatientsModule } from './patients/patients.module';
 
 @Module({
-  imports: [PatientsModule],
+  imports: [
+    PatientsModule,
+    HttpModule,
+    TypeOrmModule.forRoot(dbConfig.default),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/db-config.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+export default {
+  default: {
+    type: 'mysql',
+    name: 'default',
+    host: process.env.MYSQL_DEFAULT_HOST,
+    username: process.env.MYSQL_DEFAULT_USERNAME,
+    password: process.env.MYSQL_DEFAULT_PASSWORD,
+    database: process.env.MYSQL_DEFAULT_DATABASE,
+    entities: ['dist/entities/patients/*{.ts,.js}'],
+    synchronize: false,
+    logging: isProduction ? ['error', 'warn'] : true,
+    maxQueryExecutionTime: 1000,
+    logger: 'simple-console',
+  } as TypeOrmModuleOptions,
+};
